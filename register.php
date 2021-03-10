925fa5273430d342e0e67cc2e722b7f8abea5210
jwnukoski <jwnukoski@protonmail.com>
2021-03-09 20:54:57 -0500

    init

---
+++ register.php
@@ -0,0 +1,15 @@
+<form method="post" action="login.php">
+    <div class="form-element">
+        <label>Username:</label>
+        <input type="text" name="username" required />
+    </div>
+    <div class="form-element">
+        <label>Password:</label>
+        <input type="password" name="password" require />
+    </div>
+    <div class="form-element">
+        <label>Confirm Password:</label>
+        <input type="password" name="confirm-password" require />
+    </div>
+    <button type="submit" name="login">Log In</button>
+</form>
